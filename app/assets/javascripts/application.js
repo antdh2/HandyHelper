e244ddef741d8127f84b4d59634a20b843b8604f
Ant Horner <antdh2@gmail.com>
2016-06-18 00:33:13 +0100

    Add validations for users and listings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require toastr
 //= require turbolinks
+//= require toastr
 //= require_tree .
 
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,13 @@ class Task < ActiveRecord::Base
   #     medium: "400x400>",
   #     thumb: "100x100>" }
   # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
   mount_uploader :image, ImageUploader
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+
+  # validate presence of an image on task
+  # validates_attachment_presence :image
 
 end
